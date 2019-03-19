60dfdb801b5b9331cdf4743b70addf4a6e0eb7fa
UserFreebsd <getting1982@gmail.com>
2019-03-19 09:59:29 +0300

    Python programs

---
+++ S_treugolnika.py
@@ -0,0 +1,10 @@
+
+
+
+a = int(input())
+b = int(input())
+c = int(input())
+p = (a+b+c)/2
+p = p*((p-a)*(p-b)*(p-c))
+s = p**0.5
+print(s)
---
+++ sorted.py
@@ -0,0 +1,4 @@
+list = [int(input()) for stek in range(3)]
+list.sort()
+print(list[2], '\n', list[0], '\n', list[1])
+

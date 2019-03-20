757b1dbfd6459c3b1e514223a2d6856bab902134
UserFreebsd <getting1982@gmail.com>
2019-03-20 22:03:49 +0300

    Python

---
+++ python_part2/example.py
@@ -0,0 +1,71 @@
+"""
+import math
+x = math.sqrt(4)
+print(x)
+
+"""
+
+"""
+t = int(input("Введите время полёта :"))
+s = 539500000
+t = (t*8760)*s
+
+print("Расстояние до объекта составит :" ,t, "км")
+"""
+"""
+a=5
+while a>0:
+    print(a, end=" ")
+    a -= 1
+"""
+"""
+a = 5
+while a <=55:
+    print(a, end=" ")
+    a +=2
+"""
+
+"""
+
+i = 0
+while i <= 10:
+    i = i + 1
+    if i > 7:
+        i = i + 2
+    print(i, end=" ")
+
+"""
+"""
+n = int(input())
+c = 1
+while c <= n:
+    print("*" * c)
+    c += 1
+
+"""
+"""
+n=int(input())
+stars = "*"
+while len(stars) <= n:
+    print(stars)
+    stars += "*"
+"""
+"""
+i = 0
+while i < 5:
+    print('*')
+    if i % 2 == 0:
+        print('**')
+    if i > 2:
+        print('***')
+    i = i + 1
+"""
+
+a=3
+while a < 8:
+    print(a)
+    a=a+1
+
+
+
+
